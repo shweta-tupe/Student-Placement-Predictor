d6926f6710f56fa50c177ea55ef462fe0abc15d6
Shweta Tupe <shweta.ntupe@gmail.com>
2026-02-07 19:51:49 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,14 @@
+import os
 import pandas as pd
 import joblib
 from src.preprocess import preprocess_data
 
 def predict(input_data: dict):
-    model = joblib.load("model.pkl")
+    # Fix model path for deployment
+    BASE_DIR = os.path.dirname(__file__)           # src folder
+    MODEL_PATH = os.path.join(BASE_DIR, "..", "model.pkl")  # go up one level
+
+    model = joblib.load(MODEL_PATH)
 
     df = pd.DataFrame([input_data])
     X_scaled, _ = preprocess_data(df, training=False)
